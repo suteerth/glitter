061c09b5514090791b30dc58f04a5fbe6a37c430
suteerth <sutirthatheone@gmail.com>
2012-01-16 04:51:18 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glitter::Application.config.secret_token = '6ed780e11e3708ff29d327350ebc2cd64279d88a385a2da5d58dec89a3b666269e0ce029ba079bf8a2fca0dbc20f5182edb68125ffbd1357c4cfa83a24a61f43'
